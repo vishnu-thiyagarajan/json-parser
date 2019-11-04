a944a0475fb4133a0d9845b8c4b3bf0ce2fef3b3
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-04 09:21:59 +0530

    string parser refactored-- adding missed file

--- test.js
+++ test.js
@@ -5,69 +5,94 @@ const boolParser = input => input.startsWith('true') ?
 [true, input.slice(4)]:input.startsWith('false') ? 
 [false, input.slice(5)]:null
 
+const numParser = function (input){
+   input = input.trimStart()
+   const match = /^([+\-]?([1-9]\d*(\.\d+)?|0\.\d+|0)([eE][+\-]?\d+)?)/g.exec(input)
+   return match ?[1*match[1],input.slice(match[1].length)] : null
+}
+
+// console.log(numParser("13abc0"))
+// console.log(numParser("15.abc"))
+// console.log(numParser("1234567890"))
+// console.log(numParser("-9876.543210"))
+// console.log(numParser("0.123456789e-12"))
+// console.log(numParser("1.234567890E+34"))
+// console.log(numParser("23456789012E66"))
+// console.log(numParser("0.3.7.5"))
+// console.log(numParser("0"))
+// console.log(numParser("1"))
+// console.log(numParser("013abc"))
+// console.log(numParser("0e+-1"))
+// console.log(numParser("0e+"))
+// console.log(numParser("0e"))
+// console.log(numParser("00.15"))
+
 const strParser = function (input) {
-   if (input.startsWith('"')) {
-      let c = 0
-      let index = 1
-      for (let item = 1;item <input.length; item+=1) {
-         index += 1
-         if (input[item] === "\\"){
-            if (input[item+1] === "u"){
-               // console.log(/[0-9|A-F]{4}.*?/i.test(input.slice(item+2,item+6)))
-               if (/[0-9|A-F]{4}.*?/i.test(input.slice(item+2,item+6))){
-                  item += 5
-                  index += 5
-                  continue
-               }
-               else{
-                  return null
-               }
-            }
-            if (!['b','f','n','r','t','u','\\','"',"/"].includes(input[item+1])){
-               return null
-            }
-            c += 1
-         } else {
-            if ((c > 0 && c%2 === 0) || (input[item] === "\"" && c%2 === 0 && index !== 1)){
-               return [input.slice(0,index),input.slice(index)?input.slice(index):null]
-            }
-            if (["\t","\n"].includes(input[item])){
-               return null
-            }
-            c = 0
+   input = input.trimStart()
+   if (!input.startsWith('"')) return null
+   input = input.slice(1)
+   let c = 0
+   let index = 0
+   while (index < input.length) {
+      if (["\t","\n"].includes(input[index])) return null
+      if (input[index] === '"' && c%2 === 0) break
+      if (input[index] === "\\"){
+         c += 1
+         if (!['b','f','n','r','t','u','\\','"',"/"].includes(input[index+1])) return null
+         if (input[index+1] === "u"){
+            if (!(/[0-9|A-F]{4}.*?/i.test(input.slice(index+2,index+6)))) return null
+               index += 4
          }
       }
-      return input.endsWith("\"") ? input : null
-   } else {
-      return null
-   }
-};
-
-const numParser = function (input){
-   let match = /^([+\-]?(([1-9]\d*(\.\d+)?|0\.\d+|0$))([eE][+\-]?\d+)?)(.*?)$/g.exec(input)
-   return match ?[match[1],match[6]] : null
-}
-console.log(numParser("0.3.7"))
-console.log(numParser("13abc"))
-console.log(numParser("013abc"))
-console.log(numParser("0e+-1"))
-console.log(numParser("5e+"))
-console.log(numParser("0e"))
-console.log(numParser("15.abc"))
-console.log(numParser("1234567890"))
-console.log(numParser("-9876.543210"))
-console.log(numParser("0.123456789e-12"))
-console.log(numParser("1.234567890E+34"))
-console.log(numParser("23456789012E66"))
-console.log(numParser("0"))
-console.log(numParser("1"))
-let brace_cnt = {'{':0,'[':0}
-const arrParser = function (input){
-   
+      if (input[index] !== "\\") c = 0
+      index += 1
+      }
+   if (input[index] !== "\"") return null
+   return [input.slice(0,index),input.slice(index+1)]
 }
 
 require('fs').readFile("input.json",(err, data) => {
    if(err) throw err
-   console.log(strParser(data.toString()))
+   datas = data.toString().split("@")
+   for (let item of datas){
+      console.log(strParser(item))
+   }
 })
+
+
+
+
+
+
+
+
+// const arrParser = function (input){
+//    input = input.trim()
+//    if (!(input.startsWith("["))) return null
+//    input = input.slice(1)
+//    let index = 0
+//    while(index < input.length){
+      
+//    }
+   // elements = input.split(",")
+   // index = 0
+   // for (let item of elements){
+   //    item = item.trim()
+   //    if (index === 0){
+   //       if (!item[0] != "["){
+   //          return null
+   //       }
+   //       item = item.slice(1)
+   //    }
+   //    if (index === input.length-1){
+   //          if (!item[item.length-1] != "]"){
+   //             return null
+   //          }
+   //          item = item.slice(0,item.length-1)
+   //    }
+   // }
+   // return "passed"
+// }
+
+// console.log(arrParser('["Unclosed array", "array" , 1 , true, null]'))
 
