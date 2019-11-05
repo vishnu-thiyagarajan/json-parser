2c2a547b257acbf640e16f255932e1d96f6c6e46
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-05 17:00:06 +0530

    refactored1

--- test.js
+++ test.js
@@ -10,34 +10,30 @@ const numParser = function (input){
    return match ?[1*match[1],input.slice(match[1].length)] : null
 }
 
-const strParser = function (input) {
+const strParser = function (input,count = 0,index = 0) {
    if (!input.startsWith('"')) return null
    input = input.slice(1)
-   let c = 0
-   let index = 0
-   while (index < input.length) {
+   for (;index < input.length;index += 1) {
       if (["\t","\n"].includes(input[index])) return null
-      if (input[index] === '"' && c%2 === 0) break
+      if (input[index] === '"' && count%2 === 0) break
       if (input[index] === "\\"){
-         c += 1
+         count += 1
          if (!['b','f','n','r','t','u','\\','"',"/"].includes(input[index+1])) return null
          if (input[index+1] === "u"){
             if (!(/[0-9|A-F]{4}.*?/i.test(input.slice(index+2,index+6)))) return null
                index += 4
          }
       }
-      if (input[index] !== "\\") c = 0
-      index += 1
+      if (input[index] !== "\\") count = 0
       }
    if (input[index] !== "\"") return null
    return [input.slice(0,index),input.slice(index+1)]
 }
 
-const valParser = function (input,initial=false){
+const valParser = function (input,initial=false,result = []){
    let fnarray = [nullParser, boolParser, numParser, strParser, arrParser, objParser]
-   let result = []
-   input = input.trimStart()
    for (let item of initial ? [arrParser, objParser] : fnarray){
+      input = input.trimStart()
       result = item(input)
       if (initial && result) return !result[1] ? result[0] : null
       if (result) return result
@@ -46,7 +42,6 @@ const valParser = function (input,initial=false){
 }
 
 const arrParser = function (input){
-   input = input.trimStart()
    if (!(input.startsWith("["))) return null
    let arrresult=[],result,rest=""
    input = input.slice(1).trimStart()
@@ -58,15 +53,12 @@ const arrParser = function (input){
       rest = result[1].trimStart()
       if (rest.startsWith(']')) break
       if (!rest.startsWith(',')) return null 
-      rest = rest.slice(1)
-      input = rest
+      input = rest.slice(1)
    }
-   rest = rest.slice(1)
-   return [arrresult,rest]
+   return [arrresult,rest.slice(1)]
 }
 
 const objParser = function (input){
-   input = input.trimStart()
    if (!(input.startsWith("{"))) return null
    input = input.slice(1).trimStart()
    let objresult={},valresult,keyresult,keyrest
@@ -76,8 +68,7 @@ const objParser = function (input){
       if (!keyresult) return null
       keyrest = keyresult[1].trimStart()
       if (!keyrest.startsWith(':')) return null
-      keyrest = keyrest.slice(1)
-      valresult = valParser(keyrest)
+      valresult = valParser(keyrest.slice(1))
       if (!valresult) return null
       objresult[keyresult[0]] = valresult[0]
       input = valresult[1].trimStart()
@@ -90,10 +81,10 @@ const objParser = function (input){
 }
 
 tests = ["fail1.json","fail2.json","fail3.json","fail4.json","fail5.json","fail6.json","fail7.json","fail8.json","fail9.json","fail10.json","fail11.json","fail12.json","fail13.json","fail14.json","fail15.json","fail16.json","fail17.json","fail19.json","fail20.json","fail21.json","fail22.json","fail23.json","fail24.json","fail25.json","fail26.json","fail27.json","fail28.json","fail29.json","fail30.json","fail31.json","fail32.json","fail33.json","pass1.json","pass3.json"]
-// tests = ["fail9.json"]
 for (let test of tests){
-   require('fs').readFile("test/"+test,(err, data) => {
+   require('fs').readFile("test/"+test,'utf-8',(err, data) => {
       if(err) throw err
-      console.log(valParser(data.toString().trim(),true))
+      result = valParser(data.trim(),true)
+      console.log(result)
    })
 }
