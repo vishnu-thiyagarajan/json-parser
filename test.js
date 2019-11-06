883212296b3e6a80f3c23a1c310b0154a0cdb45d
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-06 11:59:36 +0530

    refactored obj parser

--- test.js
+++ test.js
@@ -33,8 +33,7 @@ const strParser = function (input,count = 0,index = 0) {
 const valParser = function (input,initial=false,result = []){
    let fnarray = [nullParser, boolParser, numParser, strParser, arrParser, objParser]
    for (let item of initial ? [arrParser, objParser] : fnarray){
-      input = input.trimStart()
-      result = item(input)
+      result = item(input.trimStart())
       if (initial && result) return !result[1] ? result[0] : null
       if (result) return result
    }
@@ -43,48 +42,44 @@ const valParser = function (input,initial=false,result = []){
 
 const arrParser = function (input){
    if (!(input.startsWith("["))) return null
-   let arrresult=[],result,rest=""
-   input = input.slice(1).trimStart()
-   if (input.startsWith(']')) return [arrresult, input.slice(1)]
-   while (input){
-      result = valParser(input)
+   let arrresult=[],result
+   let rest = input.slice(1).trimStart()
+   while (!rest.startsWith(']')){
+      result = valParser(rest)
       if (!result) return null
       arrresult.push(result[0])
       rest = result[1].trimStart()
-      if (rest.startsWith(']')) break
-      if (!rest.startsWith(',')) return null 
-      input = rest.slice(1)
+      if (!rest.startsWith(',')) break
+      rest = rest.slice(1)
+      if (rest.startsWith(']')) return null
    }
-   return [arrresult,rest.slice(1)]
+   return rest.startsWith(']') ? [arrresult,rest.slice(1)] : null
 }
 
 const objParser = function (input){
    if (!(input.startsWith("{"))) return null
    input = input.slice(1).trimStart()
-   let objresult={},valresult,keyresult,keyrest
+   if ((input.startsWith("}"))) return [{},input.slice(1)]
+   let objresult={},count=0,valresult,key,keyrest
    while (input){
-      if (input.startsWith('}')) break
-      keyresult = strParser(input)
-      if (!keyresult) return null
-      keyrest = keyresult[1].trimStart()
-      if (!keyrest.startsWith(':')) return null
-      valresult = valParser(keyrest.slice(1))
-      if (!valresult) return null
-      objresult[keyresult[0]] = valresult[0]
-      input = valresult[1].trimStart()
-      if (input.startsWith('}')) break
-      if (!input.startsWith(',')) return null
+      result = count%2 === 0 ? strParser(input) : valParser(input)
+      if (!result) return null
+      if (count%2 === 0) key = result[0]
+      if (count%2 !== 0) objresult[key] = result[0]
+      input = result[1].trimStart()
+      if (count%2 && input.startsWith("}")) break
+      if (!input.startsWith(count%2 === 0 ? ':' : ',')) return null
       input = input.slice(1).trimStart()
-      if (input.startsWith('}')) return null
+      count +=1
    }
    return input ? [objresult,input.slice(1)] : null
 }
 
 tests = ["fail1.json","fail2.json","fail3.json","fail4.json","fail5.json","fail6.json","fail7.json","fail8.json","fail9.json","fail10.json","fail11.json","fail12.json","fail13.json","fail14.json","fail15.json","fail16.json","fail17.json","fail19.json","fail20.json","fail21.json","fail22.json","fail23.json","fail24.json","fail25.json","fail26.json","fail27.json","fail28.json","fail29.json","fail30.json","fail31.json","fail32.json","fail33.json","pass1.json","pass3.json"]
+// tests = ["input.json"]
 for (let test of tests){
    require('fs').readFile("test/"+test,'utf-8',(err, data) => {
       if(err) throw err
-      result = valParser(data.trim(),true)
-      console.log(result)
+      console.log(valParser(data.trim(),true))
    })
 }
