cb1e905951e78b2f4a50b4440ce7f477e03b94de
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-07 22:58:28 +0530

    started with lisp arithmetic

--- test-1.js
+++ test-1.js
@@ -1,48 +1,24 @@
-const articles = [
-    {title:'First article'},
-    {title:'Second article'}
- ];
- function getArticles(){
-    setTimeout(()=>{
-       let articleContent='';
-       articles.forEach((article,index)=>{
-          articleContent+=`${article.title}`+` `;
-       });
-       console.log(articleContent);
-    },3000);
-    console.log("this is executed")
- };
- function createArticle(article){
-    return new Promise((resolve,reject)=>{
-       articles.push(article);
-       let isError=false;
-       if(!isError){
-          console.log(resolve())
-       } else {
-          reject("err");
-       }
-    });
- }
-
-
-//  createArticle({title:'Third article'})
-//  .then(10,console.log);
-
-
-// async function testAsync(){
-//     await createArticle({title:'Third article'});
-//     getArticles();
-//  }
-//  testAsync();
-
-const p1 = Promise.resolve(21);
-const p2 = 110470116021;
-const p3 = new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-    setTimeout(() => {
-        resolve('AppDividend');
-    }, 1000);
-});
-
-Promise.all([]).then(values => { 
-    console.log(values);
-});
+function execute(input,expression="",result=""){
+   let count=input.length
+   if (!input.startsWith("(")) return null
+   input = input.slice(1)
+   let operand = input[0]
+   if (!/^(\+|-|\*|\/|=|>|<|>=|<=|&|\||%|!|\^|\(|\))$/.test(operand)) return null
+   if (input[1] !== " ") return null
+   input = input.slice(2)
+   while(!input.startsWith(")")){
+      if (input[0] === "(") {
+         let value = [...execute(input)]
+         console.log(value)
+         expression += String(value[0])
+         input = value[1]
+      }
+      if (input[0] === " ") expression += operand
+      if (input[0] !== " " && input[0] !== ")") expression += input[0]
+      if (input[0] !== ")") input = input.slice(1)
+   }
+   input = input.slice(1)
+   console.log(expression)
+   return [eval(expression),input]
+}
+console.log(execute("(+ 1 (* 5 (* 6 2 (- 2 2)) 1) 5)"))
