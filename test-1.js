388ec1979683310e359081bc840b75a6af0a4b8f
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-08 11:01:35 +0530

    fixing space validations in arithmetic

--- test-1.js
+++ test-1.js
@@ -4,12 +4,12 @@ function execute(input,expression="",result=""){
    input = input.slice(1)
    let operand = input[0], number = ""
    if (input[1] === "=") {operand += "="; input = input.slice(1)}
-   if (!/^(\+|-|\*|\/|=|>|<|>=|<=|&|\||%|!|\^|\(|\))$/.test(operand)) return null
+   if (!/^(\+|-|\*|\/|=|>|<|>=|<=|&|\||%|!|!=|\^|\(|\))$/.test(operand)) return null
    if (input[1] !== " ") return null
    input = input.slice(2)
    while(!input.startsWith(")") && input.length){
       if (input[0] === "(") {
-         if (!expression.endsWith(operand)) return null
+         if (!expression.endsWith(operand)) expression += operand
          let value = execute(input)
          if (!value) return null
          expression += String(value[0])
@@ -17,7 +17,7 @@ function execute(input,expression="",result=""){
       }
       if (input[0] === " ") {
          if (number && !/^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/.test(number.trim())) return null
-         expression += operand
+         if (!expression.endsWith(operand)) expression += operand
          number = ""
       }
       if (input[0] !== " " && input[0] !== ")"){number+=input[0]; expression += input[0]}
@@ -26,9 +26,10 @@ function execute(input,expression="",result=""){
    if (number && !/^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/.test(number.trim())) return null
    if (expression.endsWith(operand)) expression = expression.slice(0,-1)
    if (!input.startsWith(")")) return null
-   input = input.slice(1)
+   input = " "+input.slice(1)
    return [eval(expression),input]
 }
-console.log(execute("(+ 1 (* 5 (* 6 2 (+ 2 2 2)) 1) 5) rest"))
-console.log(execute("(+ 3 12 )"))
+console.log(execute("(+ 1(* 5(* 6 2(+ 2 2 2))      1)5) rest"))
+console.log(execute("(+ 3      12 )"))
 console.log(execute("(<= 3 12 12)"))
+console.log(execute("(!= 4 12)"))
