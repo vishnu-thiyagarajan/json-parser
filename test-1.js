7c1cf060cc4fe49a67c84bfa8fc302de9e98e1b6
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2019-11-08 14:27:41 +0530

    rewritting arithmetic

--- test-1.js
+++ test-1.js
@@ -1,7 +1,7 @@
 function execute(input,expression="",result=""){
    let count=input.length
    if (!input.startsWith("(")) return null
-   input = input.slice(1)
+   input = input.slice(1).trim()
    let operand = input[0], number = ""
    if (input[1] === "=") {operand += "="; input = input.slice(1)}
    if (!/^(\+|-|\*|\/|=|>|<|>=|<=|&|\||%|!|!=|\^|\(|\))$/.test(operand)) return null
@@ -29,7 +29,49 @@ function execute(input,expression="",result=""){
    input = " "+input.slice(1)
    return [eval(expression),input]
 }
-console.log(execute("(+ 1(* 5(* 6 2(+ 2 2 2))      1)5) rest"))
-console.log(execute("(+ 3      12 )"))
-console.log(execute("(<= 3 12 12)"))
-console.log(execute("(!= 4 12)"))
+// console.log(execute("(+ 1(  * 5   (* 6 2(+ 2 2 2))      1)5) rest"))
+// console.log(execute("(+ 3      12 )"))
+// console.log(execute("(+ 3 12 12)"))
+// console.log(execute("(!= 4 12)"))
+function atom(str){
+   try {return parseFloat(str)}
+   catch (err){
+      try{return parseInt(str)}
+      catch(err){
+         return str
+      }
+   }
+}
+
+env_dict = {
+   '+':(input)=>input.reduce((a,b) => a+b,0),
+   '-':(input)=>input.reduce((a,b) => a-b,0),
+   '*':(input)=>input.reduce((a,b) => a*b),
+   '/':(input)=>input.reduce((a,b) => a/b),
+}
+
+function parser(input,initial=true){
+   let arr = [],operand=""
+   if (!input.startsWith("(")) return null
+   input = input.slice(1).trimStart()
+   while(!input.startsWith(")") && input.length){
+      let match = /(^[^\(\)\s]+)(.*)/.exec(input)
+      if (!match) return null
+      if (!initial) {arr.push(atom(match[1]))}
+      if (initial) {
+         if (!match || !env_dict.hasOwnProperty(match[1])) return null
+         operand = match[1]; initial = false;
+      }
+      input = match[2] ? match[2].trimStart() : ""
+      if (input.startsWith("(")){
+         let res = parser(input)
+         if (!res) return null
+         arr.push(atom(res[0]))
+         input = res[1].trimStart()
+      }
+   }
+   return [env_dict[operand](arr),input.slice(1)]
+}
+// console.log(parser("(* 2 (+ 3 (+ 2 3 -1)))"))
+// console.log(parser("(- 2 )"))
+// console.log(parser("(+ 1( * 5   (* 6 2 (/ 2 2 2))      1)5)"))
